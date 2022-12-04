f2bb35c94276b0c895547d26c6c0a79663729fec
laisha <laishamelina@gmail.com>
2022-12-03 22:35:25 -0600

    Commit creación de activities para mostrar información (03.12.22)

---
+++ app/src/main/java/com/example/proyectoahorromovil/Mostrar/AhorrosActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoahorromovil.Mostrar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.proyectoahorromovil.R;
+
+public class AhorrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahorros);
+    }
+}
